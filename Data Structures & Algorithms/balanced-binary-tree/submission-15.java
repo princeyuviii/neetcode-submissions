44f1ad08e136baf1c00e271a7bdefb772e8db3e9
Yuvraj singh rathore <yuvisinghrathore678678@gmail.com>
2026-07-23 17:04:58 +0530

    Add: balanced-binary-tree - submission-15

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-15.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root) != -1;
+    }
+
+    private int dfs(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+
+        int lh = dfs(root.left);
+        if(lh==-1){
+            return -1;
+        }
+
+        int rh = dfs(root.right);
+        if(rh==-1){
+            return -1;
+        }
+
+        if(Math.abs(lh-rh)>1){
+            return -1;
+        }
+
+        return 1 + Math.max(lh,rh);
+    }
+}
