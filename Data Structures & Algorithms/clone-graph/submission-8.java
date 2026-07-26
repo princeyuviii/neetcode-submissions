1e903e14d32caf63fb6328b9e28079f96ef56d15
Yuvraj Singh Rathore <yuvisinghrathore678678@gmail.com>
2026-07-26 16:29:26 +0530

    Add: clone-graph - submission-8

---
+++ Data Structures & Algorithms/clone-graph/submission-8.java
@@ -0,0 +1,47 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+
+    private HashMap<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if(node==null){
+            return null;
+        }
+
+        return dfs(node);
+    }
+
+    private Node dfs(Node node){
+        if(map.containsKey(node)){
+            return map.get(node);
+        }
+
+        Node copy = new Node(node.val);
+        map.put(node,copy);
+
+        for( Node neighbor : node.neighbors){
+            copy.neighbors.add(dfs(neighbor));
+        }
+
+        return copy;
+    }
+}
