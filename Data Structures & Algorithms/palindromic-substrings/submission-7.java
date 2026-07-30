c351c0087b4d60f7413a341d940d78c6b9ab3754
Yuvraj Singh Rathore <yuvisinghrathore678678@gmail.com>
2026-07-30 20:05:23 +0530

    Add: palindromic-substrings - submission-7

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-7.java
@@ -0,0 +1,19 @@
+class Solution {
+    int count = 0;
+    public int countSubstrings(String s) {
+        for(int i=0 ; i<s.length(); i++){
+            ex(s,i,i);
+            ex(s,i,i+1);
+        }
+
+        return count;
+    }
+
+    private void ex(String s, int l, int r){
+        while((l>=0 && r<s.length()) && s.charAt(l) == s.charAt(r)){
+            l--;
+            r++;
+            count++;
+        }
+    }
+}
